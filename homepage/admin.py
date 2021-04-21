3abf49f1b979afe9065ee09c8d8e873611bdf625
Manojlovic1998 <manojlovic911@hotmail.com>
2021-04-22 00:11:47 +0200

    Register db models in admin.py

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import BlogPost, Tag, Category, PostCategory, PostTag
 
-# Register your models here.
+
+class PostCategoryAdmin(admin.TabularInline):
+    model = PostCategory
+    readonly_fields = ['post', 'category']
+
+
+class PostTagAdmin(admin.TabularInline):
+    model = PostTag
+    readonly_fields = ['post', 'tag']
+
+
+class TagAdmin(admin.ModelAdmin):
+    ordering = ['title']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ['title']
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    inlines = (PostCategoryAdmin, PostTagAdmin)
+    ordering = ['published_at']
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
