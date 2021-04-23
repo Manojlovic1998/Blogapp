ccd8d08ffe3d5b09038057609cc65c780908d6f6
Manojlovic1998 <manojlovic911@hotmail.com>
2021-04-24 00:28:04 +0200

    Add CDN Bootstrap to base template, Render home page

---
+++ homepage/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import render_home_page
+urlpatterns = [
+    path('', render_home_page, name='home_page'),
+]
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def render_home_page(request):
+    """
+
+    :param request:
+    :return:
+    """
+    return render(request, 'homepage/homepage.html')
